586e850096dcae43013b2665f2a61ab8cf4480e3
yoruck2 <yoruck2@gmail.com>
2024-06-14 18:43:38 +0900

    ✨ 자주 쓰는 상수 관리를 위한 Constants  파일 추가 Color, Font enum 추가

--- Meaning Out/Constants.swift
+++ Meaning Out/Constants.swift
@@ -5,4 +5,22 @@
 //  Created by dopamint on 6/14/24.
 //
 
-import Foundation
+import UIKit
+
+enum Color {
+    
+    static let mainColor: UIColor = UIColor(named: "mainColor") ?? .tintColor
+    static let mainColorSelected: UIColor = UIColor(named: "mainColorSelected") ?? .tintColor
+    static let background: UIColor = .white
+    static let black: UIColor = .black
+    static let darkerGray: UIColor = UIColor(named: "darkerGray") ?? .tintColor
+    static let mediumGray: UIColor = UIColor(named: "mediumGray") ?? .tintColor
+    static let lighterGray: UIColor = UIColor(named: "lightGray") ?? .tintColor
+}
+
+enum Font {
+    
+    static let mainTitle = UIFont(name: "AlfaSlabOne-Regular", size: 40)
+    static let bold15 = UIFont.boldSystemFont(ofSize: 16)
+    static let bold13 = UIFont.boldSystemFont(ofSize: 13)
+}
