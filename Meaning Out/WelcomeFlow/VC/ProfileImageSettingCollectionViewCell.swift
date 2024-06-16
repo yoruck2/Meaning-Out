c254b267204744f4b5ac2e953fae5927621be1af
yoruck2 <yoruck2@gmail.com>
2024-06-16 18:21:25 +0900

    ✨ ProfileImageSettingCollectionViewCell 추가

--- Meaning Out/WelcomeFlow/VC/ProfileImageSettingCollectionViewCell.swift
+++ Meaning Out/WelcomeFlow/VC/ProfileImageSettingCollectionViewCell.swift
@@ -10,7 +10,37 @@ import UIKit
 import SnapKit
 import Then
 
-class ProfileImageSettingCollectionViewCell: UICollectionViewCell {
+class ProfileImageSettingCollectionViewCell: UICollectionViewCell, Configurable {
+    
+    var imageNumber = 0
+    var profileImageView = ProfileSettingCircleView(frame: .zero)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureHierachy()
+        configureLayout()
+        configureUI()
+    }
     
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureUI() {
+        print(imageNumber)
+        profileImageView.image = UIImage(named: "profile_" + "\(imageNumber)")
+    }
+    
+    func configureHierachy() {
+        contentView.addSubview(profileImageView)
+    }
+    
+    func configureLayout() {
+        profileImageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
 }
+
+
