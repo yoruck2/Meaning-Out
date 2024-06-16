cd8684406e58c2c50adb73767532f29dbbe06891
yoruck2 <yoruck2@gmail.com>
2024-06-16 18:16:13 +0900

    ✨ border 라인 수정을 위한 UITextField 익스텐션 추가

--- Meaning Out/Extension/TextField+.swift
+++ Meaning Out/Extension/TextField+.swift
@@ -5,4 +5,20 @@
 //  Created by dopamint on 6/14/24.
 //
 
-import Foundation
+import UIKit
+
+extension UITextField {
+    func addBottomBorder(with color: UIColor?, width borderWidth: CGFloat) {
+        let border = UIView()
+        border.autoresizingMask = [.flexibleWidth, .flexibleTopMargin]
+        border.backgroundColor = color
+        border.frame = CGRect(x: 0, 
+                              y: frame.height - borderWidth,
+                              width: frame.width,
+                              height: borderWidth)
+        
+        self.addSubview(border)
+    }
+}
+
+
