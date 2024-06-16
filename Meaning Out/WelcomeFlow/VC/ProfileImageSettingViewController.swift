9c794f4d76d2bb4628b2a27d9dcd209e3ef7236f
yoruck2 <yoruck2@gmail.com>
2024-06-16 18:21:49 +0900

    ✨ ProfileImageSettingViewController 에 profileImageSelectionCollectionView 추가

--- Meaning Out/WelcomeFlow/VC/ProfileImageSettingViewController.swift
+++ Meaning Out/WelcomeFlow/VC/ProfileImageSettingViewController.swift
@@ -10,23 +10,100 @@ import UIKit
 import SnapKit
 import Then
 
+
 class ProfileImageSettingViewController: MeaningOutViewController, Configurable {
+ 
+    let imageRange = 0...11
     
     let profileCircleView = ProfileCircleView()
     
-    let profileImageSelectionCollectionView = 
+    lazy var profileImageSettingCollectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewLayout())
     
+    func collectionViewLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewFlowLayout()
+        let sectionSpacing: CGFloat = 5
+        let cellSpacing: CGFloat = 10
+        let lineSpacing: CGFloat = 15
+        let width = UIScreen.main.bounds.width - (sectionSpacing * 2) - (cellSpacing * 3)
+        layout.itemSize = CGSize(width: width / 4.8, height: width / 4.8)
+        layout.scrollDirection = .vertical
+        layout.minimumInteritemSpacing = cellSpacing
+        layout.minimumLineSpacing = lineSpacing
+        layout.sectionInset = UIEdgeInsets(top: sectionSpacing, left: sectionSpacing, bottom: sectionSpacing, right: sectionSpacing)
+        return layout
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        configureUI()
+        configureHierachy()
+        configureLayout()
+        configureCollectionView()
+        
     }
     
     func configureHierachy() {
         view.addSubview(profileCircleView)
+        view.addSubview(profileImageSettingCollectionView)
     }
     
     func configureLayout() {
-        <#code#>
+        profileCircleView.snp.makeConstraints {
+            $0.centerX.equalTo(view.safeAreaLayoutGuide)
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(30)
+            $0.width.equalTo(view.safeAreaLayoutGuide).multipliedBy(0.3)
+            $0.height.equalTo(profileCircleView.snp.width)
+        }
+        
+        profileImageSettingCollectionView.snp.makeConstraints {
+            $0.top.equalTo(profileCircleView.snp.bottom).offset(50)
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+        
     }
     
+    func configureUI() {
+        navigationItem.title = "PROFILE SETTING"
+    }
+}
+
+extension ProfileImageSettingViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func configureCollectionView() {
+        profileImageSettingCollectionView.delegate = self
+        profileImageSettingCollectionView.dataSource = self
+        profileImageSettingCollectionView.register(ProfileImageSettingCollectionViewCell.self, forCellWithReuseIdentifier: ProfileImageSettingCollectionViewCell.id)
+    }
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        12
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ProfileImageSettingCollectionViewCell.id, for: indexPath) as! ProfileImageSettingCollectionViewCell
+        
+        print(indexPath.row)
+        print(indexPath.item)
+        print(indexPath.row)
+        
+        cell.profileImageView.image = UIImage(named: "profile_" + "\(indexPath.item)")
+//        cell.imageNumber = indexPath.item
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath) as! ProfileImageSettingCollectionViewCell
+        
+        profileCircleView.profileImageView.innerImageView.image = cell.profileImageView.image
+    }
+}
+
+extension ProfileImageSettingViewController {
+    func transfer(data: UIImage) {
+        print(#function)
+//        profileCircleView.profileImageView.innerImageView.image = data
+    }
 }
