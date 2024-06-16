21b8700e2347731e594b8fa4d4497c5a5d1168c5
yoruck2 <yoruck2@gmail.com>
2024-06-16 18:15:22 +0900

    🩹 불필요한 코드 제거

--- Meaning Out/Constants.swift
+++ Meaning Out/Constants.swift
@@ -7,20 +7,9 @@
 
 import UIKit
 
-enum Color {
-    
-    static let mainColor: UIColor = UIColor(named: "mainColor") ?? .tintColor
-    static let mainColorSelected: UIColor = UIColor(named: "mainColorSelected") ?? .tintColor
-    static let background: UIColor = .white
-    static let black: UIColor = .black
-    static let darkerGray: UIColor = UIColor(named: "darkerGray") ?? .tintColor
-    static let mediumGray: UIColor = UIColor(named: "mediumGray") ?? .tintColor
-    static let lighterGray: UIColor = UIColor(named: "lightGray") ?? .tintColor
-}
-
 enum Font {
     
     static let mainTitle = UIFont(name: "AlfaSlabOne-Regular", size: 40)
-    static let bold15 = UIFont.boldSystemFont(ofSize: 16)
+    static let bold16 = UIFont.systemFont(ofSize: 16, weight: .heavy)
     static let bold13 = UIFont.boldSystemFont(ofSize: 13)
 }
