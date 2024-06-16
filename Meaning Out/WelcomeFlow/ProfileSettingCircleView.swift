90c5206eb9c2ed41e1237994870f91aa273bf07c
yoruck2 <yoruck2@gmail.com>
2024-06-16 18:20:58 +0900

    💄

--- Meaning Out/WelcomeFlow/ProfileSettingCircleView.swift
+++ Meaning Out/WelcomeFlow/ProfileSettingCircleView.swift
@@ -5,4 +5,53 @@
 //  Created by dopamint on 6/16/24.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+
+//protocol ProfileDelegate: AnyObject {
+//    func transfer(data: UIImage)
+//}
+
+class ProfileSettingCircleView: UIImageView {
+    
+    override var isHighlighted: Bool {
+        
+        
+        didSet {
+            print("isHighlighted 바낌")
+            if isHighlighted {
+                print("회색으로")
+                //                delegate?.transfer(data: image ?? UIImage())
+                layer.borderColor = UIColor(resource: .lighterGray).cgColor
+                layer.borderWidth = 1
+                alpha = 0.5
+            } else {
+                print("하이라이트")
+                layer.borderColor = UIColor(resource: .main).cgColor
+                layer.borderWidth = 3
+                alpha = 1
+            }
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+//        image = UIImage(resource: .profile1)
+        layer.borderColor = UIColor(resource: .main).cgColor
+        layer.borderWidth = 3
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        layer.cornerRadius = self.bounds.width / 2
+        clipsToBounds = true
+        
+    }
+}
+
--- Meaning Out/WelcomeFlow/RoundView.swift
+++ Meaning Out/WelcomeFlow/RoundView.swift
@@ -5,4 +5,33 @@
 //  Created by dopamint on 6/14/24.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+
+class RoundView: UIView {
+    
+    lazy var innerImageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFill
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(innerImageView)
+        innerImageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        self.layer.cornerRadius = self.bounds.width / 2
+        self.clipsToBounds = true
+    }
+
+}
