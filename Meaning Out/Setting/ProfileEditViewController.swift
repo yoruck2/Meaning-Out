fee45ad0ff42ac0d158236a63ca32934183e606c
yoruck2 <yoruck2@gmail.com>
2024-06-23 00:11:22 +0900

    ✨ ProfileEditViewController 구현 (ProfileSettingViewController 상속 후 navigationBarItem 추가)

--- Meaning Out/Setting/ProfileEditViewController.swift
+++ Meaning Out/Setting/ProfileEditViewController.swift
@@ -5,4 +5,30 @@
 //  Created by dopamint on 6/18/24.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+
+class ProfileEditViewController: ProfileSettingViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        completeButton.isHidden = true
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "저장",
+                                                           style: .plain,
+                                                           target: self,
+                                                           action: #selector(saveButtonTapped))
+    }
+    
+    @objc
+    func saveButtonTapped() {
+        if NicknameGuide.validNickname == validateNickName(nicknameTextField.text ?? "") {
+            UserDefaultsHelper.standard.nickname = nicknameTextField.text ?? ""
+            UserDefaultsHelper.standard.profileImage = profileCircleView.profileImageView.innerImageView.image ?? UIImage()
+            self.navigationController?.popViewController(animated: true)
+        } else {
+            nicknameGuideLabel.text = "저장 안됨!!!"
+        }
+    }
+}
