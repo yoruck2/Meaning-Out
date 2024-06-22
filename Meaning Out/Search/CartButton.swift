25b8d1c34d1c35dc91ffc6810c6d976867039e08
yoruck2 <yoruck2@gmail.com>
2024-06-23 00:07:25 +0900

    ✨ 상품 상제페이지를 위한 ProductDetailViewController 구현

--- Meaning Out/Search/CartButton.swift
+++ Meaning Out/Search/CartButton.swift
@@ -5,4 +5,69 @@
 //  Created by dopamint on 6/18/24.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+
+protocol CartListCountDelegate {
+    func updateCartListCount()
+}
+
+class CartButton: UIButton {
+    
+    var delegate: CartListCountDelegate?
+    var cellProductID: String
+    
+    override var isSelected: Bool {
+        didSet {
+            if self.isSelected == true {
+                print(UserDefaultsHelper.standard.cartList)
+                print(cellProductID)
+//                if cartListData?[cellProductID] != nil {
+                var tempList = UserDefaultsHelper.standard.cartList
+                tempList.updateValue(true, forKey: cellProductID)
+                UserDefaultsHelper.standard.cartList = tempList
+
+            } else {
+                print(UserDefaultsHelper.standard.cartList)
+                print(cellProductID)
+                var tempList = UserDefaultsHelper.standard.cartList
+                tempList.removeValue(forKey: cellProductID)
+                UserDefaultsHelper.standard.cartList = tempList
+            }
+        }
+    }
+    
+    init(cellProductID: String) {
+//        print(cellProductID)
+        self.cellProductID = cellProductID
+        super.init(frame: .zero)
+        configureUI()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureUI() {
+        
+        backgroundColor = .white
+        layer.cornerRadius = 5
+        clipsToBounds = true
+        
+        contentMode = .center
+        setImage(.likeUnselected, for: .normal)
+        setImage(.likeSelected, for: .selected)
+        
+        addTarget(self, action: #selector(cartButtonTapped), for: .touchUpInside)
+        
+       
+    }
+    
+    @objc
+    func cartButtonTapped() {
+        self.isSelected.toggle()
+    }
+}
--- Meaning Out/Search/ProductDetailViewController.swift
+++ Meaning Out/Search/ProductDetailViewController.swift
@@ -6,24 +6,49 @@
 //
 
 import UIKit
+import WebKit
 
-class ProductDetailViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class ProductDetailViewController: MeaningOutViewController {
+    
+    var produtName: String
+    var productID: String
+    var productDetailUrl: String
+    
+    let webView: WKWebView = WKWebView()
+    
+    init(_ produtName: String, _ productDetailUrl: String, id productID: String) {
+        self.produtName = produtName
+        self.productDetailUrl = productDetailUrl
+        self.productID = productID
+        super.init(nibName: nil, bundle: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = produtName
+        configureWebView()
+        configureUI()
+    }
+    
+    override func configureNavigationBar() {
+        let barButton = CartButton(cellProductID: productID).toBarButtonItem()
+        navigationItem.rightBarButtonItem = barButton
+    }
+    func configureUI() {
+        view.addSubview(webView)
+        webView.snp.makeConstraints {
+            $0.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    func configureWebView() {
+        guard let url = URL(string: productDetailUrl) else {
+            return
+        }
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
 }
