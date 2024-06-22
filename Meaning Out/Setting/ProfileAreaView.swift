96ca4f65e8acc25c6d22bffe1bd86e06d4ebbfe4
yoruck2 <yoruck2@gmail.com>
2024-06-23 00:10:20 +0900

    🩹 레이아웃 수정

--- Meaning Out/Setting/ProfileAreaView.swift
+++ Meaning Out/Setting/ProfileAreaView.swift
@@ -13,22 +13,31 @@ import Then
 class ProfileAreaView: UIView, Configurable {
     
     lazy var profileCircleView = ProfileSettingCircleView(frame: .zero).then {
+        $0.image = UserDefaultsHelper.standard.profileImage
         $0.isHighlighted = true
     }
     
     let nicknameLabel = UILabel().then {
+        $0.text = UserDefaultsHelper.standard.nickname
         $0.font = Font.bold16
     }
     let signUpDate = UILabel().then {
+        $0.text = UserDefaultsHelper.standard.signUpDate + " 가입"
         $0.font = Font.bold13
         $0.textColor = .mediumGray
     }
+    
+    lazy var stackView = UIStackView(arrangedSubviews: [nicknameLabel, signUpDate]).then {
+        $0.axis = .vertical
+        $0.alignment = .fill
+        $0.distribution = .fillProportionally
+        $0.spacing = 4
+    }
+    
     let indicator = UIImageView().then {
         $0.image = UIImage(systemName: "chevron.forward")
         $0.tintColor = .darkerGray
     }
-    
-    let settingMenuTableView = UITableView()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -42,39 +51,24 @@ class ProfileAreaView: UIView, Configurable {
     }
     func configureHierachy() {
         addSubview(profileCircleView)
-        addSubview(nicknameLabel)
-        addSubview(signUpDate)
+        addSubview(stackView)
         addSubview(indicator)
-        addSubview(settingMenuTableView)
     }
     
     func configureLayout() {
         profileCircleView.snp.makeConstraints {
-//            $0.centerY.equalToSuperview()
             $0.leading.equalToSuperview().inset(10)
-            $0.verticalEdges.equalToSuperview().inset(30)
+            $0.verticalEdges.equalToSuperview().inset(15)
             $0.width.equalTo(profileCircleView.snp.height)
         }
         
-        nicknameLabel.snp.makeConstraints {
-            $0.
+        stackView.snp.makeConstraints {
+            $0.leading.equalTo(profileCircleView.snp.trailing).offset(20)
+            $0.centerY.equalToSuperview()
         }
-        
-        signUpDate.snp.makeConstraints {
-            $0.
-        }
-        
         indicator.snp.makeConstraints {
-            $0.
+            $0.trailing.equalToSuperview().inset(10)
+            $0.centerY.equalToSuperview()
         }
-        
-        settingMenuTableView.snp.makeConstraints {
-            $0.
-        }
-        
-        
-        
     }
-    
-    
 }
