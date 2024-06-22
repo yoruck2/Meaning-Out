6ed5304d89ad56fd478b66bfc3269fab4d6e9827
yoruck2 <yoruck2@gmail.com>
2024-06-23 00:11:40 +0900

    ✨ SettingMenuTableViewCell 구현

--- Meaning Out/Setting/SettingMenuTableViewCell.swift
+++ Meaning Out/Setting/SettingMenuTableViewCell.swift
@@ -5,4 +5,54 @@
 //  Created by dopamint on 6/18/24.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+
+class SettingMenuTableViewCell: UITableViewCell, Configurable {
+    
+    let menuLabel = UILabel().then {
+        $0.font = Font.medium16
+    }
+    
+    let cartListCountLabel = UILabel().then {
+        let cartCount = UserDefaultsHelper.standard.cartList.count
+        $0.isHidden = true
+        let attributedString = NSMutableAttributedString()
+        let imageAttachment = NSTextAttachment()
+        imageAttachment.image = UIImage(resource: .likeSelected)
+        imageAttachment.bounds = CGRect(x: 0, y: -5, width: 22, height: 22)
+        attributedString.append(NSAttributedString(attachment: imageAttachment))
+        attributedString.append(NSAttributedString(string: " \(cartCount)개의 상품"))
+        attributedString.addAttribute(.font, value: Font.bold16, range: NSRange(location: 0, length: 4))
+        
+        $0.attributedText = attributedString
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureHierachy()
+        configureLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureHierachy() {
+        contentView.addSubview(menuLabel)
+        contentView.addSubview(cartListCountLabel)
+    }
+    
+    func configureLayout() {
+        menuLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().inset(10)
+        }
+        cartListCountLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(10)
+        }
+    }
+}
