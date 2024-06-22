918ff9f7c89a45717f8da08050690899fbd09cf5
yoruck2 <yoruck2@gmail.com>
2024-06-23 00:12:12 +0900

    ✨ SettingViewController 구현

--- Meaning Out/Setting/SettingViewController.swift
+++ Meaning Out/Setting/SettingViewController.swift
@@ -12,36 +12,124 @@ import Then
 
 class SettingViewController: MeaningOutViewController, Configurable {
     
+    lazy var profileAreaView = ProfileAreaView().then {
+        let tapImageViewRecognizer = UITapGestureRecognizer(target: self,
+                                                            action: #selector(profileTapped))
+        $0.isUserInteractionEnabled = true
+        $0.addGestureRecognizer(tapImageViewRecognizer)
+    }
+    let settingMenuTableView = UITableView()
     
-    
-    
-    
-//    @objc
-//    func imageTapped(tapGestureRecognizer: UITapGestureRecognizer) {
-//        
-//        let nextVC = ProfileImageSettingViewController()
-//        
-//        nextVC.profileCircleView.profileImageView.innerImageView.image = profileCircleView.image
-//        nextVC.delegate = self
-//        navigationController?.pushViewController(nextVC, animated: true)
-//    }
+    @objc
+    func profileTapped(tapGestureRecognizer: UITapGestureRecognizer) {
+        let nextVC = ProfileEditViewController()
+        navigationController?.pushViewController(nextVC, animated: true)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
+        configureHierachy()
+        configureLayout()
+        configureTableView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        profileAreaView.profileCircleView.image = UserDefaultsHelper.standard.profileImage
+        profileAreaView.nicknameLabel.text = UserDefaultsHelper.standard.nickname
     }
     
     func configureUI() {
         navigationItem.title = "SETTING"
+        navigationItem.leftBarButtonItem = nil
     }
     func configureHierachy() {
-         
+        view.addSubview(profileAreaView)
+        view.addSubview(settingMenuTableView)
     }
     
     func configureLayout() {
-         
+        profileAreaView.snp.makeConstraints {
+            $0.horizontalEdges.top.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(100)
+        }
+        
+        settingMenuTableView.snp.makeConstraints {
+            $0.horizontalEdges.bottom.equalTo(view.safeAreaLayoutGuide)
+            $0.top.equalTo(profileAreaView.snp.bottom)
+        }
     }
     
+    func configureTableView() {
+        settingMenuTableView.separatorStyle = .singleLine
+        settingMenuTableView.separatorColor = .darkerGray
+        settingMenuTableView.delegate = self
+        settingMenuTableView.dataSource = self
+        settingMenuTableView.register(SettingMenuTableViewCell.self,
+                                      forCellReuseIdentifier: SettingMenuTableViewCell.id)
+    }
+    
+    func showAlert(completion: @escaping (UIAlertAction) -> Void) {
+        let alert = UIAlertController(title: "탈퇴하기",
+                                      message: "탈퇴를 하면 데이터가 모두 초기화됩니다. 탈퇴 하시겠습니까?",
+                                      preferredStyle: .alert)
+        let ok = UIAlertAction(title: "확인",
+                               style: .destructive,
+                               handler: completion)
+        let cancel = UIAlertAction(title: "취소",
+                                   style: .cancel)
+        alert.addAction(ok)
+        alert.addAction(cancel)
+        
+        self.present(alert, animated: true)
+    }
+    
+    func removeUserData() {
+        for key in UserDefaultsHelper.standard.userDefaults.dictionaryRepresentation().keys {
+            UserDefaults.standard.removeObject(forKey: key.description)
+        }
+    }
+}
+
+extension SettingViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    enum SettingMenu: String, CaseIterable {
+        case myCartList = "나의 장바구니 목록"
+        case FAQ = "자주 묻는 질문"
+        case QnA = "1:1 문의"
+        case NotificationSetting = "알림 설정"
+        case withdraw = "탈퇴하기"
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        SettingMenu.allCases.count
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.section == 4 {
+            showAlert() {_ in
+                self.removeUserData()
+                
+                let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
+                let sceneDelegate = windowScene?.delegate as? SceneDelegate
+                let vc = UINavigationController(rootViewController: WelcomeViewController())
+                sceneDelegate?.changeRootVC(vc, animated: true)
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: SettingMenuTableViewCell.id,
+                                                 for: indexPath) as! SettingMenuTableViewCell
+        if indexPath.section == 0 {
+            cell.cartListCountLabel.isHidden = false
+        }
+        cell.menuLabel.text = SettingMenu.allCases[indexPath.section].rawValue
+        return cell
+    }
 }
 
 extension SettingViewController: ProfileImageSettingDelegate {
