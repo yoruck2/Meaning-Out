71c1ef804d247981c22644aafc213b9d6fed99f7
yoruck2 <yoruck2@gmail.com>
2024-06-24 23:23:01 +0900

    ✨ AlertType enum 추가, showAlert extension으로 분리

--- Meaning Out/Extension/UIViewController+.swift
+++ Meaning Out/Extension/UIViewController+.swift
@@ -7,6 +7,17 @@
 
 import UIKit
 
+enum AlertType: String {
+    case withdrawal = "탈퇴하기"
+    
+    var message: String {
+        switch self {
+        case .withdrawal:
+            return "탈퇴를 하면 데이터가 모두 초기화됩니다. 탈퇴 하시겠습니까?"
+        }
+    }
+}
+
 extension UIViewController {
     func hideKeyboardWhenTappedAround() {
         let tap = UITapGestureRecognizer(target: self, 
@@ -17,5 +28,20 @@ extension UIViewController {
     
     @objc func dismissKeyboard() {
         view.endEditing(true)
+    }
+    
+    func showAlert(alerType: AlertType, completion: @escaping (UIAlertAction) -> Void) {
+        let alert = UIAlertController(title: alerType.rawValue,
+                                      message: alerType.message,
+                                      preferredStyle: .alert)
+        let ok = UIAlertAction(title: "확인",
+                               style: .destructive,
+                               handler: completion)
+        let cancel = UIAlertAction(title: "취소",
+                                   style: .cancel)
+        alert.addAction(ok)
+        alert.addAction(cancel)
+        
+        self.present(alert, animated: true)
     }
 }
