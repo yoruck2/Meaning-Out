2849fc0bdc7c7acb1876b8ace15ec462b4ff641a
yoruck2 <yoruck2@gmail.com>
2024-07-02 01:30:38 +0900

    ✨ AlertType 케이스 추가, Alert 메서드 세분화

--- Meaning Out/Extension/UIViewController+.swift
+++ Meaning Out/Extension/UIViewController+.swift
@@ -7,13 +7,25 @@
 
 import UIKit
 
-enum AlertType: String {
-    case withdrawal = "탈퇴하기"
+enum AlertType {
+    case withdrawal
+    case networkError(error: String)
+    
+    var title: String {
+        switch self {
+        case .withdrawal:
+            return "탈퇴하기"
+        case .networkError:
+            return "네트워크 오류"
+        }
+    }
     
     var message: String {
         switch self {
         case .withdrawal:
             return "탈퇴를 하면 데이터가 모두 초기화됩니다. 탈퇴 하시겠습니까?"
+        case .networkError(let error):
+            return "\(error)\n다시 시도해주세요"
         }
     }
 }
@@ -30,13 +42,22 @@ extension UIViewController {
         view.endEditing(true)
     }
     
-    func showAlert(alertType: AlertType, completion: @escaping (UIAlertAction) -> Void) {
-        let alert = UIAlertController(title: alertType.rawValue,
+    func showConfirmCancelAlert(alertType: AlertType, completion: @escaping (UIAlertAction) -> Void) {
+        let alert = UIAlertController(title: alertType.title,
                                       message: alertType.message,
                                       preferredStyle: .alert)
         
         alert.addAction("확인", .destructive, handler: completion)
         alert.addAction("취소", .cancel)
+        self.present(alert, animated: true)
+    }
+    
+    func showConfirmAlert(alertType: AlertType) {
+        let alert = UIAlertController(title: alertType.title,
+                                      message: alertType.message,
+                                      preferredStyle: .alert)
+        
+        alert.addAction("확인", .default)
         self.present(alert, animated: true)
     }
 }
