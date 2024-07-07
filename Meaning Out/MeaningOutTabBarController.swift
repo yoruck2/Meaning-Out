ace65f71434662efe4adf3706bd6db3d7b74db94
yoruck2 <yoruck2@gmail.com>
2024-07-08 07:05:28 +0900

    ✨ 탭바 컨트롤 추가

--- Meaning Out/MeaningOutTabBarController.swift
+++ Meaning Out/MeaningOutTabBarController.swift
@@ -11,20 +11,22 @@ final class MeaningOutTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let firstVC = UINavigationController(rootViewController: SearchViewController())
-        let secondVC = UINavigationController(rootViewController: SettingViewController())
+        let searchVC = UINavigationController(rootViewController: SearchViewController())
+        let wishListVC = UINavigationController(rootViewController: WishlistViewController())
+        let settingVC = UINavigationController(rootViewController: SettingViewController())
 
-        setViewControllers([firstVC, secondVC],
-                                            animated: true)
+        setViewControllers([searchVC, settingVC], animated: true)
         tabBar.tintColor = .main
         tabBar.backgroundColor = .white
         
         if let items = tabBar.items {
             items[0].image = UIImage(systemName: "magnifyingglass")
             items[0].title = "검색"
-        
-            items[1].image = UIImage(systemName: "person")
+            items[1].image = UIImage(systemName: "heart")
+            items[1].selectedImage = UIImage(systemName: "heart.fillheart")
             items[1].title = "설정"
+            items[2].image = UIImage(systemName: "person")
+            items[2].title = "설정"
         }
     }
 
