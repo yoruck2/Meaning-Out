ecbf653d24d3575eef8388c04bfba2031f550e05
yoruck2 <yoruck2@gmail.com>
2024-07-08 07:06:12 +0900

    🚧 WishlistViewController 구현진행중

--- Meaning Out/Wishlist/WishlistViewController.swift
+++ Meaning Out/Wishlist/WishlistViewController.swift
@@ -5,4 +5,54 @@
 //  Created by dopamint on 7/8/24.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+
+class WishlistViewController: UIViewController, UISearchControllerDelegate {
+    
+    let repository = WishListTableRepository.shared
+    
+    let productSearchBar = UISearchController().then {
+        $0.searchBar.placeholder = "브랜드, 상품 등을 입력하세요."
+    }
+    let wishlistCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionView.collectionViewLayout())
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+    
+    func configureHierarchy() {
+        view.addSubview(productSearchBar.searchBar)
+        view.addSubview(wishlistCollectionView)
+    }
+    func configureLayout() {
+        wishlistCollectionView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    func configureView() {
+        navigationItem.searchController = productSearchBar
+        navigationItem.searchController?.hidesNavigationBarDuringPresentation = true
+        navigationItem.hidesSearchBarWhenScrolling = false
+        navigationItem.searchController?.obscuresBackgroundDuringPresentation = false
+        navigationItem.leftBarButtonItem = nil
+        productSearchBar.delegate = self
+    }
+}
+
+extension WishlistViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        repository.wishlistTable.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+    }
+    
+    
+}
